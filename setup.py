54becd01599c8bebb1e95fe795c65193332f1dc0
nethish <nethish259@gmail.com>
2024-03-08 21:02:20 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='casparser',
+    packages=find_packages(include=['casparser']),
+    version='0.1.0',
+    description='Cas summary parser',
+    author='Nethish Rajendran',
+    author_email='nethish259@gmail.com',
+    install_requires=['tabula-py==2.9.0', 'jpype1==1.5.0'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==8.0.2'],
+    test_suite='tests',
+)
